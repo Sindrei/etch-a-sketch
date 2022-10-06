e83f5e7c304d71e82da713c223f821edecd7bf96
Francois Malan <francoisdaniel.malan@gmail.com>
2022-10-06 10:58:36 +0200

    Changed the grid items to be dynamic
    Grid items will now automatically adjust their size to fit into the
    provided canvas space of 600 x 600 pixels.
    
    User is also restricted to adding no more than 100 grid items per side.

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 // Creates a grid of div according to the specified size
-function createGrid() {
+function createGrid(size = 16) {
   const gridContainer = document.querySelector(".grid-container");
-  for (let i = 1; i <= 16; i++) {
+  for (let i = 1; i <= size; i++) {
     const gridRow = document.createElement("div");
     gridRow.classList.toggle("grid-row");
     gridContainer.appendChild(gridRow);
-    for (let y = 0; y < 16; y++) {
+    for (let y = 1; y <= size; y++) {
       const gridSquare = document.createElement("div");
       gridSquare.classList.toggle("grid-item");
+      gridSquare.addEventListener("mouseover", () => {
+        gridSquare.classList.add("grid-item-hover");
+      });
       gridRow.appendChild(gridSquare);
     }
   }
 }
 
+// Creates the initial default grid at size 16 x 16
 createGrid();
 
-// Selects all the grid items
-const items = Array.from(document.querySelectorAll(".grid-item"));
-// Adding event listeners to each item in the gride for mouse hover
-// Adding class grid-item-hover when mouse over and removing when mouse leaves
-items.forEach((item) => {
-  item.addEventListener("mouseover", () => {
-    item.classList.add("grid-item-hover");
-  });
-  item.addEventListener("mouseout", () => {
-    item.classList.remove("grid-item-hover");
-  });
+const newGrid = document.querySelector(".button-new");
+// Event listener to create a new grid
+newGrid.addEventListener("click", () => {
+  removeGrid();
+  let size = 0;
+  do {
+    size = prompt("Please enter the side length of the grid");
+  } while (size > 100);
+  createGrid(size);
 });
+
+function removeGrid() {
+  const rows = Array.from(document.querySelectorAll(".grid-row"));
+  const gridContainer = document.querySelector(".grid-container");
+  rows.forEach((row) => {
+    gridContainer.removeChild(row);
+  });
+}
